70c37c40b6dc29d640f11b07bc35279ab1da6432
javier roman <22670018@itiguala.edu.mx>
2023-03-10 07:37:20 -0600

    se crea la clase paciente

--- src/datosNutriologo.java
+++ src/datosNutriologo.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class datosNutriologo {
-}
+import java.util.Scanner;
+public class datosNutriologo {
+    private String nombre;
+    private int edad;
+    private String sexo;
+    private double estatura;
+    private double Peso;
+
+    public datosNutriologo(String nombre, int edad, String sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public datosNutriologo() {
+
+    }
+
+    public datosNutriologo(String nombre, int edad, String sexo, double estatura, double peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.estatura = estatura;
+        Peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(double peso) {
+        Peso = peso;
+    }
+    public void imc(){
+        double imc=getPeso()/(getEstatura()*getEstatura());
+        if ((imc<18.50) || (imc <24.99)){
+            System.out.println("Peso normal"
+                    +"\nIMC obtenido:"+imc
+                    +"\nnombre:"+getNombre());
+        }else if((imc<25.00) || (imc<29.99)){
+            System.out.println("esta en sobrepeso;"
+                    +"\nIMC obtenido:"+imc
+                    +"\nnombre"+getNombre());
+        }
+    }
+
+
+
+    }
+
+
+
--- src/vistaDeDatos.java
+++ src/vistaDeDatos.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class vistaDeDatos {
-}
+import java.util.Scanner;
+
+public class vistaDeDatos {
+    public static void main(String[] args) {
+
+        datosNutriologo persona = new datosNutriologo("javier",18,"m");//Creamos una instancia de la clase
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("introduce el peso");
+            persona.setPeso(sc.nextDouble());
+            System.out.println("Introduce estatura");
+            persona.setEstatura(sc.nextDouble());
+
+        persona.imc();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
